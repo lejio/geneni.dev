a2955bd323c55e2e450813909d116692601a6e5d
Gene Ni <geneisjuan@gmail.com>
2025-06-30 11:14:47 -0400

    added motion one and started overhaul on experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,32 +1,105 @@
-import * as React from "react";
+import { useEffect } from "react";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../components/ui/Carousel";
+import { animate, stagger } from "framer-motion";
+// import MSU_Helmet from "../images/msu_helmet.svg";
+// import MSU_Seal from "../images/msu_seal.svg";
 
+import I_C from "../images/I_C";
+import I_CPP from "../images/I_CPP";
 import JHUAPL from "./JHUAPL";
-import MSU from "./MSU";
+// import MSU from "./MSU";
 
 export default function Experience() {
+  useEffect(() => {
+    const elements = Array.from(document.querySelectorAll(".stagger"));
+    const controls = animate(
+      elements,
+      { opacity: [0, 1], y: [20, 0] },
+      {
+        delay: stagger(0.1, { startDelay: 0.2 }),
+        duration: 0.4,
+        ease: "easeOut",
+      }
+    );
+    return () => {
+      controls.stop();
+    };
+  }, []);
   // #052f73
   return (
-    <Carousel className="max-w-[1920px] max-h-[1080px] w-full h-[85vh] flex flex-col items-center justify-center">
-      <CarouselContent>
-        <CarouselItem className="w-full">
-          <div className="p-5 h-full bg-gradient-to-bl from-green-400 to-[#18453B] rounded-xl">
-            <MSU />
+    <div className="relative max-w-[1920px] md:max-h-[1080px] w-full h-auto md:h-[85vh] flex flex-col gap-5 rounded-xl md:border md:border-solid md:border-[#18453B] items-center align-middle">
+      <div className="absolute inset-0 p-10">
+        <img
+          src="/msu_helmet.svg"
+          alt="Background Logo"
+          className="w-full h-full object-contain opacity-[0.10] z-0 pointer-events-none"
+        />
+      </div>
+
+      <div className="relative z-10 p-5 w-full h-full flex flex-col justify-evenly items-center align-middle">
+        <div className="flex flex-col justify-center items-center align-middle gap-2">
+          <img
+            src="/msu_seal.svg"
+            alt="MSU Seal"
+            className="stagger w-16 h-16 md:w-32 md:h-32"
+          />
+          <h1 className="stagger text-base md:text-xl text-[#18453B] font-bold">
+            Michigan State University
+          </h1>
+          <p className="stagger text-sm md:text-base text-[#18453B]">
+            Irrigation Laboratory
+          </p>
+        </div>
+
+        <div className="flex w-full h-full pt-5 flex-row  gap-5">
+          <div className="flex flex-col rounded w-full h-full justify-start items-center align-middle gap-2">
+            <div className="flex flex-row justify-center gap-5 w-full">
+              <img
+                src="/MSU_1.jpg"
+                alt="LOCOMOS Sensor"
+                className="h-52 rounded"
+              />
+              <div className="flex flex-col gap-2">
+                <h2 className="text-base md:text-md text-[#18453B] font-bold">
+                  Low-Cost Monitoring System (LOCOMOS)
+                </h2>
+                <hr />
+                <div className="flex flex-row justify-start">
+                  <div className="h-16 w-16">
+                    <I_C />
+                  </div>
+                  <div className="h-16 w-16">
+                    <I_CPP />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </CarouselItem>
-        <CarouselItem className="w-full">
-          <div className="p-5 h-full bg-gradient-to-bl from-[#052f73] to-blue-400 rounded-xl">
-            <JHUAPL />
-          </div>
-        </CarouselItem>
-      </CarouselContent>
-    </Carousel>
+          <div className="flex flex-col rounded w-full h-full bg-green-200 justify-center items-center align-middle gap-2"></div>
+          <div className="flex flex-col rounded w-full h-full bg-green-200 justify-center items-center align-middle gap-2"></div>
+          <div className="flex flex-col rounded w-full h-full bg-green-200 justify-center items-center align-middle gap-2"></div>
+        </div>
+      </div>
+    </div>
   );
 }
+
+// export default function Experience() {
+//   // #052f73
+//   return (
+//     <Carousel className="max-w-[1920px] max-h-[1080px] w-full h-[85vh] flex flex-col items-center justify-center">
+//       <CarouselContent>
+//         <CarouselItem className="w-full">
+//           {/* <div className="p-5 h-full bg-gradient-to-bl from-green-400 to-[#18453B] rounded-xl"> */}
+//           <MSU />
+//           {/* </div> */}
+//         </CarouselItem>
+//         <CarouselItem className="w-full">
+//           <div className="p-5 h-full bg-gradient-to-bl from-[#052f73] to-blue-400 rounded-xl">
+//             <JHUAPL />
+//           </div>
+//         </CarouselItem>
+//       </CarouselContent>
+//     </Carousel>
+//   );
+// }
--- src/components/MSU.tsx
+++ src/components/MSU.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import msu_icon from "../images/msu_helmet.svg";
-import MSU_1 from "../images/MSU_1.jpg";
-import MSU_2 from "../images/MSU_2.jpg";
-import MSU_3 from "../images/MSU_3.jpg";
-import MSU_4 from "../images/MSU_4.jpg";
+// import React from "react";
+// import msu_icon from "../images/msu_helmet.svg";
+// import MSU_1 from "../images/MSU_1.jpg";
+// import MSU_2 from "../images/MSU_2.jpg";
+// import MSU_3 from "../images/MSU_3.jpg";
+// import MSU_4 from "../images/MSU_4.jpg";
 
-import I_CPP from "../images/I_CPP";
-import I_Astro from "../images/I_Astro";
-import I_TS from "../images/I_TS";
-import I_C from "../images/I_C";
+// import I_CPP from "../images/I_CPP";
+// import I_Astro from "../images/I_Astro";
+// import I_TS from "../images/I_TS";
+// import I_C from "../images/I_C";
 
-export default function MSU() {
-  const techStack_1 = [I_CPP, I_C];
-  const techStack_2 = []
+// export default function MSU() {
+//   const techStack_1 = [I_CPP, I_C];
+//   const techStack_2 = []
 
-  return (
-    <div className="w-full h-full flex flex-col justify-start align-middle items-center gap-5">
-      <div className="flex text-[#18453B] flex-col justify-center items-center align-middle">
-        <img className="w-10 md:w-20" src={msu_icon.src} alt="MSU Logo" />
-        <div className="flex flex-col justify-center items-center align-middle">
-          <h2 className="text-base md:text-xl">Michigan State University</h2>
-          <p className="text-sm">Irrigation Laboratory</p>
-        </div>
-      </div>
+//   return (
+//     <div className="w-full h-full flex flex-col justify-start align-middle items-center gap-5">
+//       <div className="flex text-[#18453B] flex-col justify-center items-center align-middle">
+//         <img className="w-10 md:w-20" src={msu_icon.src} alt="MSU Logo" />
+//         <div className="flex flex-col justify-center items-center align-middle">
+//           <h2 className="text-base md:text-xl">Michigan State University</h2>
+//           <p className="text-sm">Irrigation Laboratory</p>
+//         </div>
+//       </div>
 
-      <div className=" text-white flex flex-col gap-5 h-full">
-        <div className=" w-[300px] md:w-[600px] flex flex-row gap-5">
-          <img
-            className="w-28 md:h-auto md:w-64 rounded-xl object-cover"
-            src={MSU_1.src}
-            alt="Pic at MSU"
-          />
-          <div className="w-full flex flex-col gap-2">
-            <h2 className="text-sm md:text-xl">Full Stack Developer</h2>
-            <div className="flex flex-row gap-2">
-              {techStack_1.map((Icon, idx) => {
-                return (
-                  <div key={idx} className="w-6 h-6 md:w-10 md:h-10">
-                    <Icon />
-                  </div>
-                );
-              })}
-            </div>
-            <p className=" text-xs md:text-base text-wrap">
-                Programmed the LOCOMOS line of sensors that are being used across Michigan, and now Washington State. Improved battery life by 100% with ULP mode and local caching, allowing sensors to survive the winter during low sunlight seasons.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+//       <div className=" text-white flex flex-col gap-5 h-full">
+//         <div className=" w-[300px] md:w-[600px] flex flex-row gap-5">
+//           <img
+//             className="w-28 md:h-auto md:w-64 rounded-xl object-cover"
+//             src={MSU_1.src}
+//             alt="Pic at MSU"
+//           />
+//           <div className="w-full flex flex-col gap-2">
+//             <h2 className="text-sm md:text-xl">Full Stack Developer</h2>
+//             <div className="flex flex-row gap-2">
+//               {techStack_1.map((Icon, idx) => {
+//                 return (
+//                   <div key={idx} className="w-6 h-6 md:w-10 md:h-10">
+//                     <Icon />
+//                   </div>
+//                 );
+//               })}
+//             </div>
+//             <p className=" text-xs md:text-base text-wrap">
+//                 Programmed the LOCOMOS line of sensors that are being used across Michigan, and now Washington State. Improved battery life by 100% with ULP mode and local caching, allowing sensors to survive the winter during low sunlight seasons.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
